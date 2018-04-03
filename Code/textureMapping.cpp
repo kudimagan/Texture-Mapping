a9f0b34e4f449b3b4989a11f095bf8315e709435
TheGrayFrost <vishesh0512@gmail.com>
2018-04-03 13:59:09 +0530

    Textures

---
+++ Code/textureMapping.cpp
@@ -0,0 +1,300 @@
+#include "graphicsLibrary.h"
+
+using namespace objl;
+
+#define scale 1000
+#define DIV 40
+#define MULT 0.4
+#define width 200
+#define height 200
+
+map <int, Vector3> vertices;
+map <int, triangle> objMap;
+map < int, vector <int> > vertexTriangle;
+int vertexCount;
+int triangleCount;
+Vector3 mean;
+double ka, kd, ks, spep;	// material reflective properties
+double d0, d1, d2;			// attenuation
+double Ia, Ip;				// ambient light and light source
+
+Vector3 calculateMean()
+{
+	Vector3 minv, maxv;
+	for (int it = 1; it <= vertexCount; it++)
+	{
+		minv = math::min (minv, vertices[it]);
+		maxv = math::max (maxv, vertices[it]);
+	}
+	return ((minv + maxv) / 2);
+}
+
+void shiftScale ()
+{
+	for (int it = 1; it <= vertexCount; ++it)
+		vertices[it] = vertices[it] - mean;
+}
+
+void OBJParse (string filename)
+{
+	ifstream file(filename);
+	string curline;
+	int counter = 1;
+	double minX, maxX, minY, maxY, minZ, maxZ;
+	bool flag = false;
+
+	while (getline(file, curline))
+	{
+		if (algorithm::firstToken(curline) == "v")
+		{
+			vector<string> spos;
+			Vector3 vpos;
+			algorithm::split(algorithm::tail(curline), spos, " ");
+			vpos.X = stod(spos[0]) * scale + 150;
+			vpos.Y = stod(spos[1]) * scale;
+			vpos.Z = stod(spos[2]) * scale - 400;
+			vertices[counter++] = vpos;
+			vertexCount++;
+			flag = true;
+		}
+
+		else if (flag)
+			break;
+	}
+
+	mean = calculateMean();
+	//shiftScale();
+
+	do
+	{
+		if (algorithm::firstToken(curline) == "f")
+		{
+			vector<string> spos;
+			Vector3 vpos;
+			int v1, v2, v3;
+			algorithm::split(algorithm::tail(curline), spos, " ");
+			v1 = stoi(spos[0]);
+			v2 = stoi(spos[1]);
+			v3 = stoi(spos[2]);
+			triangle t;
+			t.v[0] = vertices[v1];
+			t.v[1] = vertices[v2];
+			t.v[2] = vertices[v3];
+			t.n = math::CrossV3(t.v[1]-t.v[0], t.v[2]-t.v[0]);
+			t.n.UnifyV3();
+			t.centroid = (t.v[0] + t.v[1] + t.v[2]) / 3;
+			t.index[0] = v1;
+			t.index[1] = v2;
+			t.index[2] = v3;
+			vertexTriangle[v1].push_back(triangleCount);
+			vertexTriangle[v2].push_back(triangleCount);
+			vertexTriangle[v3].push_back(triangleCount);
+			objMap[triangleCount++] = t;
+		}
+	} while (getline(file, curline));
+
+	//cout << "Vertices: " << vertexCount << "\n";
+	//cout << "Faces: " << triangleCount << "\n";
+}
+
+void getNormals()
+{
+	for (int it = 1; it <= vertexCount; it++)
+	{
+		int temp = vertexTriangle[it].size();
+		Vector3 nn;
+		for (int it1 = 0; it1 < temp; it1++)
+			nn = nn + objMap[vertexTriangle[it][it1]].n;
+		nn.UnifyV3();
+		//cout<<nn.X<<" "<<nn.Y<<" "<<nn.Z<<endl;
+		for (int it1 = 0; it1 < temp; it1++)
+		{
+			for (int q = 0; q < 3; q++)
+			{
+				if (objMap[vertexTriangle[it][it1]].index[q] == it)
+				{
+					objMap[vertexTriangle[it][it1]].n1[q] = nn;
+					break;
+				}
+			}
+		}
+	}
+}
+
+colourRGB trace (Vector3 point, Vector3 dir, colourRGB lightColorRGB, Vector3 lightloc, int mode, colourRGB * image2 = NULL, int width1 = 0, int height1 = 0)
+{
+	colourHSV surface;
+	colourRGB sRGB;
+	surface = convertRGB2HSV(lightColorRGB * Ip);
+	surface.v = 0;
+
+	double f, att, ang, minZ = INF;
+	Vector3 minPoint(-INF,-INF,-INF);
+	int minTri = -1;
+	double t, u, v;
+
+	// Z-buffer
+	for (int it = 0; it < triangleCount; it++)
+	{	 
+		if (objMap[it].visible)
+		{
+			if (algorithm::rayTriangleIntersect(point, dir, objMap[it].v[0], objMap[it].v[1], objMap[it].v[2], t, u, v))
+			{
+				Vector3 pp = objMap[it].v[0] * (1-u-v) + objMap[it].v[1] * u + objMap[it].v[2] * v;
+				if (pp.Z < minZ)
+				{
+					minZ = pp.Z;
+					minPoint = pp;
+					minTri = it;
+				}
+			}
+		}
+	}
+
+	if ((minZ == INF) || (minTri == -1))
+	{
+		sRGB = convertHSV2RGB (surface);
+		//sRGB = sRGB + colourRGB(Ia, Ia, Ia) * ka;
+		return sRGB;
+	}
+
+	else 
+	{	
+		if (mode == 1)
+		{
+			Vector3 director = minPoint - mean;
+			director.UnifyV3();
+			double ut = sqrt(director.X * director.X + director.Z * director.Z);
+			double theta = acos(director.X / ut);
+			double phi = acos(ut);
+			if (director.Z < 0)
+				theta = 2 * M_PI - theta;
+			if (director.Y < 0)
+				phi = 2 * M_PI - phi;
+			theta /= (2 * M_PI / DIV);
+			phi /= (2 * M_PI / DIV);
+			surface.v = ((int)theta + (int)phi + 1) % 2;
+		}
+		else if (mode == 2)
+		{
+			Vector3 director = minPoint - mean;
+			double r = sqrt(director.X * director.X + director.Z * director.Z);
+			double theta = acos(director.X / r);
+			if (director.Z < 0)
+				theta = 2 * M_PI - theta;
+			theta /= (2 * M_PI / DIV);
+			r /= DIV;
+			surface.v = ((int)theta + (int)(director.Y / (MULT * DIV))) % 2;
+			//cout << director.Y << "\n";
+		}
+		else if (mode == 3)
+			surface.v = ((int)(minPoint.X / (MULT * DIV)) + (int)(minPoint.Y / (MULT * DIV))) % 2;
+
+		Vector3 lcap = lightloc - minPoint;
+		f = math::MagnitudeV3(lcap);
+		lcap = lcap / f;
+		Vector3 ncap = objMap[minTri].n1[0]*(1-u-v)+objMap[minTri].n1[1]*u+objMap[minTri].n1[2]*v;
+		ncap.UnifyV3();
+		ang = math::DotV3 (ncap, lcap);
+		Vector3 rcap = 2 * (ang * ncap) - lcap;
+		Vector3 vcap = dir;
+		vcap.UnifyV3();
+		att = 1.0 / (d0 + d1 * f + d2 * f*f);
+
+		surface.v = surface.v * (att * (kd * ang + ks * pow(math::DotV3(rcap, vcap), spep)));
+	}
+
+	// Ambient light
+	sRGB = convertHSV2RGB (surface);
+	sRGB = sRGB + colourRGB(Ia, Ia, Ia) * ka;
+
+	return sRGB;
+}
+
+//removing directly invisible faces
+void backFaceCulling (Vector3 viewer)
+{
+	for (int it = 0; it < triangleCount; it++)
+	{
+		Vector3 v1 = objMap[it].centroid - viewer;
+		v1.UnifyV3();
+		double doo = math::DotV3(v1, objMap[it].n);
+		if (doo > vEpsilon)
+			objMap[it].visible = 0;
+	}
+}
+
+//renders image with texture
+//mode = 0 => No texture
+//mode = 1 => Spherical Texture
+//mode = 2 => Cylindrical Texture
+//mode = 3 => Planar Texture
+//mode = 4 => Cubic Texture
+void render (string filename, int mode = 0)
+{
+	colourRGB * image = new colourRGB[width * height];
+	colourRGB * pixel = image;
+	
+	//Back Face Culling
+	Vector3 viewer(50, 50, 1000);
+	backFaceCulling(viewer);
+
+	// Processing texture
+	cout << "Rendering Image:" << filename << "\nProgress...\n";
+	for (int j = 0; j < height; j++)
+	{
+		for(int i = 0; i < width; i++, pixel++)
+		{
+			Vector3 framePt(i, height-1-j, 0);
+			Vector3 lightLoc(300,300,300);
+			Vector3 dir = framePt - viewer;
+			//*pixel = trace (viewer, dir, colourRGB(0,0,1), lightLoc, image2, width1, height1);
+			*pixel = trace (viewer, dir, colourRGB(1,0,0), lightLoc, mode);
+			//fill colour at pixel
+			progressBar (((j+1) * height + (i + 1)) * 1.0 / (width * height));
+		}
+	}
+
+	double maxim = 0;
+	for (int i = 0; i < width * height; ++i)
+	{
+		maxim = max(maxim, image[i].r);
+		maxim = max(maxim, image[i].g);
+		maxim = max(maxim, image[i].b);
+	}
+
+	//Writing to file
+	cout << "\nImage file opened for writing.\n";
+	ofstream ofs (filename.c_str(), ios::out);
+	ofs << "P6\n" << width << " " << height << "\n255\n";
+	
+	for (unsigned i = 0; i < width * height; ++i) 
+		ofs << (unsigned char)((image[i].r / maxim) * 255) 
+			<< (unsigned char)((image[i].g / maxim) * 255) 
+			<< (unsigned char)((image[i].b / maxim) * 255);
+	
+	ofs.close();
+	
+	delete [] image;
+}
+
+int main()
+{
+	ka = 1;
+	kd = 0.5;
+	ks = 0.5;
+	spep = 1;
+	d0 = 1;
+	d1 = 0;
+	d2 = 0;
+	Ia = 0.2;
+	Ip = 0.6;
+
+	OBJParse("../Objects/bunny.obj");
+	cout << "Object file parsed.\n";
+	getNormals();
+	cout << "Face normals calculated.\n";
+	render("../Images/bunny15.ppm", 2);
+	cout << "Image rendered.\n";
+	return 0;
+}
